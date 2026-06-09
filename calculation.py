0b2a07d893ad0f58f6811fd05db26d0ff47dd8cc
Zainab Noor <zainabnoor7012@gmail.com>
2026-06-09 06:30:50 -0700

    simple calculations

---
+++ calculation.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum is: ", a + b)
+print("Product is: ", a * b)
+print("Difference is: ", a - b)
---
+++ input.py
@@ -0,0 +1,3 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"{name} is {age} years old")
